f8e339af8b58719f7a767e1d944c714fb12eaea9
Andy Meyer <ameyer24@gmail.com>
2017-03-30 11:54:33 -0500

    Working on bulk ingest from a folder (and not an individual file).

--- MainRScript.R
+++ MainRScript.R
@@ -6,7 +6,51 @@
 #### Ideally would read every file from a given folder.
 #### Make the files self readable
 #### Sort each Counter Report into it's own category.
-## Test the new branch.
+#### Test the new branch.
+
+#### Using this code for this section - http://www.reed.edu/data-at-reed/resources/R/reading_and_writing.html
+
+
+## Setting up the folder pathway and creating a file_list variable.
+folder <- "C:/Users/ameyer/Desktop/CounterReports"
+file_list <- list.files(path=folder, pattern="*.csv")
+## Write some conditional test that will validate we are looking at Database Report 1
+
+## This for loop assumes that everything is a DB1 report. Maybe I could sort them first...
+
+## Try to define my own function.
+read.counter <-function(x){
+  read.csv(x,skip = 7, header=T)
+  
+}
+
+for (i in 1:length(file_list)){
+  assign(file_list[i],
+  read.counter(paste(folder, file_list[i], sep='/')))
+}
+
+
+for (i in 1:length(file_list)){
+  assign(file_list[i],
+  read.csv(paste(folder, file_list[i], sep='/'),skip = 7, header=T))
+}
+
+
+
+
+## This is good. All the files are now dataframes in R.
+## Need to clean up each of those dataframes, though.
+
+DB1 <- EBSCODB12015.csv
+DB2 <- EBSCODB12016.csv
+
+DB1$Reporting.Period.Total <-NULL
+
+
+for (i in 1:length(file_list)){
+  file_list[i]$Reporting.Period.Total <-NULL
+}
+
 
 IngestCounterDB1.DF <- read.csv("C:/Users/ameyer/Desktop/EBSCODB12016.csv",
                           skip = 7, # skip the introductions
