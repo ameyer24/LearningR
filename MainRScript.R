805566c030ec2b8a19dcfea92ac584ffc25a6f59
Andy Meyer <ameyer24@gmail.com>
2017-03-31 08:51:31 -0500

    Defined a "counter cleaner" function that works pretty well!

--- MainRScript.R
+++ MainRScript.R
@@ -11,54 +11,74 @@ library(readxl)
 
 ## Setting up the folder pathway and creating the file_list variables.
 folder <- "C:/Users/ameyer/Desktop/CounterReports"
+
 ## This sorts the Counter Reports into CSV or XL file formats.
 file_list_csv <- list.files(path=folder, pattern="*.csv")
 file_list_xl <- list.files(path=folder, pattern="*.xl*")
-##
+
+## Counting up the total number of files...
+number_of_files <- length(file_list_csv) + length(file_list_xl)
+
 
 ## This iterates through the list of CSV filese and creates dataframes for each report
 for (i in 1:length(file_list_csv)){
-  assign(paste0("CounterReport",i,".DF"), read_csv(paste(folder, file_list_csv[i], sep='/'),skip=7))
+  assign(paste0("CounterReport",i), read_csv(paste(folder, file_list_csv[i], sep='/'),skip=7))
 }
 
 ## This iterates through list of Excel files and creates dataframes for each report.
 ## Updated naming conventions to prevent overriding existing dataframes.
 for (i in 1:length(file_list_xl)){
-  assign(paste0("CounterReport",i+(length(file_list_csv)),".DF"), read_excel(paste(folder, file_list_xl[i], sep='/'),skip=7))
+  assign(paste0("CounterReport",i+(length(file_list_csv))), read_excel(paste(folder, file_list_xl[i], sep='/'),skip=7))
 }
 
+### Definign my own function.
 
-### Try to define my own function.
-# 
-# CounterCleaner <- function(x){
-#   x <- subset(x, select = -c(5))
-#   x <- gather(x, date, usage,)
-#   x
-# }
-# CLEANEDDF2 <- CounterCleaner(CounterReport2.DF)
-# CLEANEDDF2
-# 
-# ## End of my function section.
+CounterCleaner <- function(x){
+  x <- subset(x, select = -c(5))
+  x <- gather(x, date, usage, 5:ncol(x))
+  x <- separate(x, date, c("Month", "Year"))
+  return(x)
+}
+
+CounterReport1 <- CounterCleaner(CounterReport1)
+CounterReport2 <- CounterCleaner(CounterReport2)
+CounterReport3 <- CounterCleaner(CounterReport3)
+CounterReport4 <- CounterCleaner(CounterReport4)
+CounterReport5 <- CounterCleaner(CounterReport5)
+CounterReport6 <- CounterCleaner(CounterReport6)
+CounterReport7 <- CounterCleaner(CounterReport7)
+CounterReport8 <- CounterCleaner(CounterReport8)
+CounterReport9 <- CounterCleaner(CounterReport9)
+CounterReport10 <- CounterCleaner(CounterReport10)
+CounterReport11 <- CounterCleaner(CounterReport11)
+CounterReport12 <- CounterCleaner(CounterReport12)
+
 
 
 ## Remove the "Reporting Period Total" column. Because we don't need it.
 ## This is stupid. Remove by column name instead. This is dangerous!
 
+for (i in 1:10){
+  CounterReport[i] <- 1
+}
+  #CounterReport[i] <- subset(CounterReport[i], select = -c(5))
+
+
 ## Loop this somehow.
 
-CounterReport1.DF <- subset(CounterReport1.DF, select = -c(5))
-CounterReport2.DF <- subset(CounterReport2.DF, select = -c(5))
-CounterReport3.DF <- subset(CounterReport3.DF, select = -c(5))
-CounterReport4.DF <- subset(CounterReport4.DF, select = -c(5))
-CounterReport5.DF <- subset(CounterReport5.DF, select = -c(5))
-CounterReport6.DF <- subset(CounterReport6.DF, select = -c(5))
-CounterReport7.DF <- subset(CounterReport7.DF, select = -c(5))
-CounterReport8.DF <- subset(CounterReport8.DF, select = -c(5))
-CounterReport9.DF <- subset(CounterReport9.DF, select = -c(5))
-CounterReport10.DF <- subset(CounterReport10.DF, select = -c(5))
+CounterReport1 <- subset(CounterReport1, select = -c(5))
+CounterReport2 <- subset(CounterReport2, select = -c(5))
+CounterReport3 <- subset(CounterReport3, select = -c(5))
+CounterReport4 <- subset(CounterReport4, select = -c(5))
+CounterReport5 <- subset(CounterReport5, select = -c(5))
+CounterReport6 <- subset(CounterReport6, select = -c(5))
+CounterReport7 <- subset(CounterReport7, select = -c(5))
+CounterReport8 <- subset(CounterReport8, select = -c(5))
+CounterReport9 <- subset(CounterReport9, select = -c(5))
+CounterReport10 <- subset(CounterReport10, select = -c(5))
 
 ## This command gathers together the data to make one long DF that seperates each variable.
-CounterReport1.DF <- gather(CounterReport1.DF, date, usage, 5:ncol(CounterReport1.DF))
+CounterReport1 <- gather(CounterReport1.DF, date, usage, 5:ncol(CounterReport1.DF))
 CounterReport2.DF <- gather(CounterReport2.DF, date, usage, 5:ncol(CounterReport2.DF))
 CounterReport3.DF <- gather(CounterReport3.DF, date, usage, 5:ncol(CounterReport3.DF))
 CounterReport4.DF <- gather(CounterReport4.DF, date, usage, 5:ncol(CounterReport4.DF))
