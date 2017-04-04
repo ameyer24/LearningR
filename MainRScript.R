0fa8033cee248e3892abfaec42a91ae060b42883
Andy Meyer <ameyer24@gmail.com>
2017-04-04 10:25:09 -0500

    Fixed capitalization issues.

--- MainRScript.R
+++ MainRScript.R
@@ -19,7 +19,7 @@ folder <- "C:/Users/ameyer/Desktop/CounterReports"
 DB1r4_CSV_Cleaner <- function(file){
   x <- read_csv(file, skip=7, col_names = TRUE)
   x <- subset(x, select = -c(5))
-  x <- gather(x, date, usage, 5:ncol(x))
+  x <- gather(x, date, Usage, 5:ncol(x))
   x <- separate(x, date, c("Month", "Year"))
   return(x)
 }
@@ -28,7 +28,7 @@ DB1r4_CSV_Cleaner <- function(file){
 DB1r4_xl_Cleaner <- function(file){
   x <- read_excel(file, skip=7, col_names = TRUE)
   x <- subset(x, select = -c(5))
-  x <- gather(x, date, usage, 5:ncol(x))
+  x <- gather(x, date, Usage, 5:ncol(x))
   x <- separate(x, date, c("Month", "Year"))
   return(x)
 }
@@ -50,14 +50,10 @@ Tidy_DB1_data <-unique(rbind(load_csv_data(folder),load_xl_data(folder)))
 ## Change month abbreviation to number to make sorting easier.
 Tidy_DB1_data$Month <- match(tolower(Tidy_DB1_data$Month), tolower(month.abb))
 
-##############################################
+#################################################
 ##TRANSFORM AND VISUALIZE AND MODEL THE DATA.
 #################################################
-## I'm spreading the data back into a more familiar view. Things more about this.
-## Unite Year and Month to make sorting easier.
-BasicCounterReport <- unite(Tidy_DB1_data, Date, c(Year,Month), sep="-")
-BasicCounterReport <- spread(BasicCounterReport, Date, usage, convert=TRUE,fill = 0)
-BasicCounterReport  <- arrange(BasicCounterReport,Platform)
+####General Overview
 
 ## See what publishers we have in the dataset.
 unique(c(Tidy_DB1_data$Publisher))
@@ -66,15 +62,29 @@ unique(c(Tidy_DB1_data$Publisher))
 unique(c(Tidy_DB1_data$Platform))
 
 ## See what databases we have in the dataset.
-databaselist <- unique(c(Tidy_DB1_data$Database))
+unique(c(Tidy_DB1_data$Database))
+
 ## It would be useful to import pricing data about each database.
-
+## I ant to create a blank dataframe for manual data entry.
 ## This should help.
-databaselist <- unique(c(Tidy_DB1_data$Database))
-yearlist <- unique(c(Tidy_DB1_data$Year))
+## This could be improved with pipes.
+Pricing_Info <- unique(subset(Tidy_DB1_data, select=c(Database,Platform,Year)))
+Pricing_Info$Price <-"$0"
+Pricing_Info <- spread(Pricing_Info, Year, Price, convert=TRUE, fill="$0")
+Pricing_Info <- unique(Pricing_Info)  
+## Write this to CSV.
+write.csv(Pricing_Info, file="C:/Users/ameyer/Desktop/Pricing_Info.csv")
+## Expect the user to enter pricing information for each of these resources.
+
+## GRAPH THIS STUFF
+ggplot(data=Tidy_DB1_data) + geom_point(mapping = aes(x=Year, y=Usage))
 
 
-write.csv(databaselist, file="C:/Users/ameyer/Desktop/databaselist.csv")
+## I'm spreading the data back into a more familiar view. Things more about this.
+## Unite Year and Month to make sorting easier.
+BasicCounterReport <- unite(Tidy_DB1_data, Date, c(Year,Month), sep="-")
+BasicCounterReport <- spread(BasicCounterReport, Date, usage, convert=TRUE,fill = 0)
+BasicCounterReport  <- arrange(BasicCounterReport,Platform)
 
 ## Divide data into actual databases and EDS results
 ## This adds a new column for the total.
