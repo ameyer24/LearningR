100bd94a876089b02f24561fedcd9fb9c6272f73
Andy Meyer <ameyer24@gmail.com>
2017-05-19 13:50:02 -0500

    Minor changes to the import process.

--- OPAC/OPAC_log.R
+++ OPAC/OPAC_log.R
@@ -14,26 +14,32 @@ vufind.column.names <- c("AccessMethod","test","test","test","DateTime","test","
 # Testing Things Out one file at a time.
 raw.vufind.data <- read.table("Q:/OPAC_Logs/vufind_access_log.20170304.gz",
            colClasses = vufind.column.classes,
-           col.names = vufind.column.names)
+           col.names = vufind.column.names,
+           comment.char = "")
 
 NPU.vufind <- raw.vufind.data %>%
   filter(grepl('vf-npu',SearchURL)) %>%
   mutate(DateTime = as.POSIXct(DateTime,
                                tz="GMT",
-                               format="[%d/%b/%Y:%H:%M:%S")) %>%
-  filter(grepl('lookfor',SearchURL))
+                               format="[%d/%b/%Y:%H:%M:%S"))
+
+NPU.vufind.records <- NPU.vufind %>%
+  filter(grepl("Record",SearchURL))
+
+NPU.vufind.searches <- NPU.vufind %>%
+  filter(grepl("lookfor",SearchURL))
   
-## Can I read a bunch of OPAC logs? Yes! Is this smart?
-log.files <- list.files(folder, full.names = T)
-raw.vufind.data <- do.call(rbind, lapply(log.files,
-                                      read.table, 
-                                      colClasses = vufind.column.classes,
-                                      col.names = vufind.column.names)
-                        )
-
-NPU.vufind <- raw.vufind.data %>%
-  filter(grepl('vf-npu',SearchURL)) %>%
-  mutate(DateTime = as.POSIXct(DateTime,
-                               tz="GMT",
-                               format="[%d/%b/%Y:%H:%M:%S")) %>%
-  filter(grepl('lookfor',SearchURL))
+# # Can I read a bunch of OPAC logs? Yes! Is this smart?
+# log.files <- list.files(folder, full.names = T)
+# raw.vufind.data <- do.call(rbind, lapply(log.files,
+#                                       read.table, 
+#                                       colClasses = vufind.column.classes,
+#                                       col.names = vufind.column.names)
+#                         )
+# 
+# NPU.vufind <- raw.vufind.data %>%
+#   filter(grepl('vf-npu',SearchURL)) %>%
+#   mutate(DateTime = as.POSIXct(DateTime,
+#                                tz="GMT",
+#                                format="[%d/%b/%Y:%H:%M:%S")) %>%
+#   filter(grepl('lookfor',SearchURL))
