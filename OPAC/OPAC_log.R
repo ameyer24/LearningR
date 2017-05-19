2755bfac36a3bbbedb11bf66681a3257bcc76e42
Andy Meyer <ameyer24@gmail.com>
2017-05-19 11:35:12 -0500

    Cleaned up style and tidied up the code.

--- OPAC/OPAC_log.R
+++ OPAC/OPAC_log.R
@@ -1,35 +1,39 @@
 install.packages("tidyverse")
 library(tidyverse)
 
-## Setting things up
-## Define path to folder that contains the OPAC logs.
+# Setting things up
+# Define path to folder that contains the OPAC logs.
 folder <- "Q:/OPAC_Logs"
 
-## Defines the Column Classes. The "NULL" columns don't contain useful information and will be skipped.
-VuFind_Col_Classes <- c("character","NULL","NULL","NULL","character","NULL","character","NULL","NULL","NULL","character")
-## Defines the column names.
-VuFind_Col_Names <- c("AccessMethod","test","test","test","DateTime","test","SearchURL","test","test","test","Browser")
+# Defines the Column Classes.
+# The "NULL" columns don't contain useful information and will be skipped.
+vufind.column.classes <- c("character","NULL","NULL","NULL","character","NULL","character","NULL","NULL","NULL","character")
+# Defines the column names.
+vufind.column.names <- c("AccessMethod","test","test","test","DateTime","test","SearchURL","test","test","test","Browser")
 
-# ## Testing Things Out one file at a time.
-# VuFind_Data <- read.table("Q:/OPAC_Logs/vufind_access_log.20170304.gz",
-#            colClasses = VuFind_Col_Classes,
-#            col.names = VuFind_Col_Names)
-# 
-# NPU_VuFind <- VuFind_Data %>%
-#   filter(grepl('vf-npu',SearchURL)) %>%
-#   mutate(DateTime = as.POSIXct(DateTime, tz="GMT", format="[%d/%b/%Y:%H:%M:%S")) %>%
-#   filter(grepl('lookfor',SearchURL))
+# Testing Things Out one file at a time.
+raw.vufind.data <- read.table("Q:/OPAC_Logs/vufind_access_log.20170304.gz",
+           colClasses = vufind.column.classes,
+           col.names = vufind.column.names)
+
+NPU.vufind <- raw.vufind.data %>%
+  filter(grepl('vf-npu',SearchURL)) %>%
+  mutate(DateTime = as.POSIXct(DateTime,
+                               tz="GMT",
+                               format="[%d/%b/%Y:%H:%M:%S")) %>%
+  filter(grepl('lookfor',SearchURL))
   
 ## Can I read a bunch of OPAC logs? Yes! Is this smart?
-## Also good news - no need to unzip the files!
-log_files <- list.files(folder, full.names = T)
-VuFind_Data <- do.call(rbind, lapply(log_files,
+log.files <- list.files(folder, full.names = T)
+raw.vufind.data <- do.call(rbind, lapply(log.files,
                                       read.table, 
-                                      colClasses = VuFind_Col_Classes,
-                                      col.names = VuFind_Col_Names)
+                                      colClasses = vufind.column.classes,
+                                      col.names = vufind.column.names)
                         )
 
-NPU_VuFind <- VuFind_Data %>%
+NPU.vufind <- raw.vufind.data %>%
   filter(grepl('vf-npu',SearchURL)) %>%
-  mutate(DateTime = as.POSIXct(DateTime, tz="GMT", format="[%d/%b/%Y:%H:%M:%S")) %>%
+  mutate(DateTime = as.POSIXct(DateTime,
+                               tz="GMT",
+                               format="[%d/%b/%Y:%H:%M:%S")) %>%
   filter(grepl('lookfor',SearchURL))
