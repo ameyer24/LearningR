6d17612a3824095fbda5880e6ab77bd2b6d404c6
Andy Meyer <ameyer24@gmail.com>
2017-05-23 14:23:08 -0500

    Got a first attempt at the regex to parse the SearchURL string. Success!

--- OPAC/OPAC_log.R
+++ OPAC/OPAC_log.R
@@ -1,15 +1,49 @@
 install.packages("tidyverse")
+install.packages("stringr")
 library(tidyverse)
+library(stringr)
 
-# Setting things up
+
 # Define path to folder that contains the OPAC logs.
 folder <- "Q:/OPAC_Logs"
 
-# Defines the Column Classes.
-# The "NULL" columns don't contain useful information and will be skipped.
-vufind.column.classes <- c("character","NULL","NULL","NULL","character","NULL","character","NULL","NULL","NULL","character")
-# Defines the column names.
-vufind.column.names <- c("AccessMethod","test","test","test","DateTime","test","SearchURL","test","test","test","Browser")
+# Define the Column Classes. NULL columns classes are skipped.
+vufind.column.classes <- c("character",
+                           "NULL",
+                           "NULL",
+                           "NULL",
+                           "character",
+                           "NULL",
+                           "character",
+                           "NULL",
+                           "NULL",
+                           "NULL",
+                           "character")
+
+# Define the column names.
+vufind.column.names <- c("AccessMethod",
+                         "test",
+                         "test",
+                         "test",
+                         "DateTime",
+                         "test",
+                         "SearchURL",
+                         "test",
+                         "test",
+                         "test",
+                         "Browser")
+
+# Using regular expressions to parse out the SearchURL field.
+# It's a mess but it's a start!
+# Currently returns only the first five filters.
+
+Vufind.SearchURL.pattern <- "(GET) (.+[\\?])(lookfor.*?[\\&| ])?(type.*?[\\&| ])?(start.*?[\\&| ])?(submit.*?[\\&| ])?(only.*?[\\&| ])?(search.*?[\\&| ])?(filter.*?[\\&| ])?(filter.*?[\\&| ])?(filter.*?[\\&| ])?(filter.*?[\\&| ])?(filter.*?[\\&| ])?(view.*?[\\&| ])?(sort.*?[\\&| ])?(page.*?[\\&| ])?"
+
+Vufind.SearchURL.column.names <- c("cat1","c2","c3","c4","c5","c6","c7","c8","c9","c11","c11","c12","c13","c14","c15","c16","c17")
+
+###############################################################################
+# Reading the log files into R-------------------------------------------------
+###############################################################################
 
 # Testing Things Out one file at a time.
 raw.vufind.data <- read.table("Q:/OPAC_Logs/vufind_access_log.20170304.gz",
@@ -17,29 +51,31 @@ raw.vufind.data <- read.table("Q:/OPAC_Logs/vufind_access_log.20170304.gz",
            col.names = vufind.column.names,
            comment.char = "")
 
-NPU.vufind <- raw.vufind.data %>%
+# Reads all the files from the given folder.
+log.files <- list.files(folder, full.names = T)
+raw.vufind.data <- do.call(rbind, lapply(log.files,
+                                         read.table,
+                                         colClasses = vufind.column.classes,
+                                         col.names = vufind.column.names,
+                                         comment.char = ""))
+
+NPU.vufind.data <- raw.vufind.data %>%
   filter(grepl('vf-npu',SearchURL)) %>%
   mutate(DateTime = as.POSIXct(DateTime,
                                tz="GMT",
                                format="[%d/%b/%Y:%H:%M:%S"))
 
-NPU.vufind.records <- NPU.vufind %>%
-  filter(grepl("Record",SearchURL))
 
-NPU.vufind.searches <- NPU.vufind %>%
-  filter(grepl("lookfor",SearchURL))
-  
-# # Can I read a bunch of OPAC logs? Yes! Is this smart?
-# log.files <- list.files(folder, full.names = T)
-# raw.vufind.data <- do.call(rbind, lapply(log.files,
-#                                       read.table, 
-#                                       colClasses = vufind.column.classes,
-#                                       col.names = vufind.column.names)
-#                         )
-# 
-# NPU.vufind <- raw.vufind.data %>%
-#   filter(grepl('vf-npu',SearchURL)) %>%
-#   mutate(DateTime = as.POSIXct(DateTime,
-#                                tz="GMT",
-#                                format="[%d/%b/%Y:%H:%M:%S")) %>%
-#   filter(grepl('lookfor',SearchURL))
+
+NPU.vufind.searches <- NPU.vufind.data %>%
+  filter(grepl("Search/Home",SearchURL))
+
+
+# Divides the SearchURL field into parts.
+test1 <- str_match(NPU.vufind.searches$SearchURL, Vufind.SearchURL.pattern)
+test2 <- str_detect(NPU.vufind.searches$SearchURL, Vufind.SearchURL.pattern)
+test3 <- separate(data = NPU.vufind.searches,
+           col = SearchURL,
+           into = Vufind.SearchURL.column.names,
+           sep = Vufind.SearchURL.pattern)
+
