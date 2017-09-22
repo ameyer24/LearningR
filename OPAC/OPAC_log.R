8bbc5a37e1ecf9ef68713fab86c1085e7296c715
Andy Meyer <ameyer24@gmail.com>
2017-09-22 17:11:22 -0500

    New approach to splitting the SearchURL into usable parts; many mutates instead of a massive regex. Seems much better.

--- OPAC/OPAC_log.R
+++ OPAC/OPAC_log.R
@@ -73,8 +73,12 @@ read.vufind.logs <- function(folder) {
 
 raw.vufind.data <- read.vufind.logs(OPAC_folder)
 
+###############################################################################
+# Filtering the Data -------------------------------------------------
+###############################################################################
+
 # Filters out non-NPU rows; returns only rows with 'vf-npu' in the SearchURL.
-NPU.vufind.data <- filter(raw.vufind.data, grepl('vf-npu',SearchURL))
+NPU.vufind.data <- filter(raw.vufind.data, grepl("vf-npu",SearchURL))
 
 # Filters out only the lines that contain search information.
 NPU.vufind.searches <- filter(NPU.vufind.data,grepl("Search/Home",SearchURL))
@@ -82,77 +86,47 @@ NPU.vufind.searches <- filter(NPU.vufind.data,grepl("Search/Home",SearchURL))
 # Filters out only the lines that contain search information.
 NPU.vufind.records <- filter(NPU.vufind.data, grepl("vf-npu/Record",SearchURL))
 
-
-library(webreadr)
-requests <- split_clf(NPU.vufind.data$SearchURL)
-
-
-request2 <- separate(data=NPU.vufind.data, col=SearchURL, into = c("Method","SearchURL", "Protocol"), sep = " ")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 ###############################################################################
 # Dividing the SearchURL-------------------------------------------------
 ###############################################################################
 
-# Using regular expressions to parse out the SearchURL field.
-# This regex returns the entire string divided into parts.
-# Currently returns only the first five filters.
+# # Using regular expressions to parse out the SearchURL field.
+# # This regex returns the entire string divided into parts.
+# # Currently returns only the first five filters.
+# Vufind.SearchURL.regex <- "(.+[\\?])(lookfor.*?[\\&| ])?(type.*?[\\&| ])?(start.*?[\\&| ])?(submit.*?[\\&| ])?(only.*?[\\&| ])?(search.*?[\\&| ])?(filter.*?[\\&| ])?(filter.*?[\\&| ])?(filter.*?[\\&| ])?(filter.*?[\\&| ])?(filter.*?[\\&| ])?(view.*?[\\&| ])?(sort.*?[\\&| ])?(page.*?[\\&| ])?"
+# 
+# Vufind.SearchURL.column.names <- c("FirstPart",
+#                                    "LookFor",
+#                                    "Type",
+#                                    "Start",
+#                                    "Submit",
+#                                    "OnlyMine",
+#                                    "Search",
+#                                    "Filter1",
+#                                    "Filter2",
+#                                    "Filter3",
+#                                    "Filter4",
+#                                    "Filter5",
+#                                    "View",
+#                                    "Sort",
+#                                    "Page")
+# 
+# NPU.vufind.searches2 <- extract(data = NPU.vufind.searches,
+#                                 col = SearchURL,
+#                                 into = Vufind.SearchURL.column.names,
+#                                 regex = Vufind.SearchURL.regex)
+# # This effectively cleans up the junk from each column...
+# # but there must be a better way to do this.
+# NPU.vufind.searches3 <- NPU.vufind.searches2%>%
+#   mutate(LookFor = gsub("lookfor=","",LookFor)) %>%
+#   mutate(Type = gsub("type=","",Type))
 
-Vufind.SearchURL.regex <- "(GET) (.+[\\?])(lookfor.*?[\\&| ])?(type.*?[\\&| ])?(start.*?[\\&| ])?(submit.*?[\\&| ])?(only.*?[\\&| ])?(search.*?[\\&| ])?(filter.*?[\\&| ])?(filter.*?[\\&| ])?(filter.*?[\\&| ])?(filter.*?[\\&| ])?(filter.*?[\\&| ])?(view.*?[\\&| ])?(sort.*?[\\&| ])?(page.*?[\\&| ])?"
+# New approach. Extracting parts of the SearchURL in small, simple pieces.
+# I think this approach is better; easier to understand.
+# Doesn't need to be comprehensive (i.e. account for the whole SearchURL)
+# Instead needs to answer some questions about the data.
 
-# Work on improving regex to return only the important parts.
-
-Vufind.SearchURL.column.names <- c("GET",
-                                   "FirstPart",
-                                   "LookFor",
-                                   "Type",
-                                   "Start",
-                                   "Submit",
-                                   "OnlyMine",
-                                   "Search",
-                                   "Filter1",
-                                   "Filter2",
-                                   "Filter3",
-                                   "Filter4",
-                                   "Filter5",
-                                   "View",
-                                   "Sort",
-                                   "Page")
-
-
-
-###############################################################################
-# Divides the SearchURL into useful parts--------------------------------------
-###############################################################################
-
-# This creates a new dataframe with the SearchURL divided into parts.
-NPU.vufind.searches.divided <- extract(data = NPU.vufind.searches,
-                                       col = SearchURL,
-                                       into = Vufind.SearchURL.column.names,
-                                       regex = Vufind.SearchURL.regex)
-
-write.csv(NPU.vufind.searches.divided, "Q:/OPAC_Logs/NPUSearches.csv")
-
-# A clunky step to clean up the data in the divided table.
-# This can be extended to cover all cases.
-# Perhaps better way to do this.
-NPU.vufind.searches.clean <- NPU.vufind.searches.divided %>%
-  mutate(LookFor = gsub("lookfor=","",LookFor)) %>%
-  mutate(Type = gsub("type=","",Type))
-  
-write.csv(NPU.vufind.searches.clean, "C:/NPUSearches.csv")
-
+NPU.vufind.searches.test <- NPU.vufind.searches %>%
+  mutate(LookFor = str_extract(SearchURL,"(lookfor.*?[\\&| ])")) %>%
+  mutate(Type = str_extract(SearchURL,"(type.*?[\\&| ])"))
 
