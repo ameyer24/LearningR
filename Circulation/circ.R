8b5379f4951063dbe7e249d299c2cfb72cd59177
Andy Meyer <ameyer24@gmail.com>
2017-11-15 16:52:12 -0600

    Updated to the circulation report; finally got to collapse the variables to make it more helpful.

--- Circulation/circ.R
+++ Circulation/circ.R
@@ -1,17 +1,27 @@
 ###############################################################################
-# Reading the files and tidying the data ______________________________________
+# Set Up _____________________________________________________________
 ###############################################################################
-
+# I've decided to go with a data.table approach here.
+# Load Packages
+install.packages("data.table")
 library(tidyverse)
 library(lubridate)
-# Setting up the file pathway
-circ_raw <- "C:/DataScience/inputs/Circulation/AJM - Circulation for R.txt"
+library(data.table)
 
-# Setting up the file headers
+# Set up Files
+circ.transactions <- fread(C:/DataScience/inputs/Circulation/circtransactions.csv)
+
+circ_raw <- "C:/DataScience/inputs/Circulation/AJM - Circulation for R.txt"
+# Set up the Column Names for the Import File
 col_names = c("Circ_ID","Item_ID","Item_Type","Patron","Charge_Date","Due_Date")
 
+
+###############################################################################
+# Import and Tidy _____________________________________________________________
+###############################################################################
+
 # Importing the data.
-circ_data <- read.csv(file = circ_raw, col.names = circ_col_names)
+circ_data <- read.csv(file = circ_raw, col.names = col_names)
 
 # Cleaning up the date/time fields.
 circ_data$Charge_Date <- parse_date_time(circ_data$Charge_Date,
@@ -23,18 +33,30 @@ circ_data$Due_Date <- parse_date_time(circ_data$Due_Date,
 # Transformations and Visualizations __________________________________________
 ###############################################################################
 
-ggplot(circ_data, aes(x=year(Charge_Date))) +
+# Bar Plot of Circulation Transactions by Year
+ggplot(data = circ_data, aes(x=year(Charge_Date))) +
+  geom_bar()
+
+# Bar Plot of Circulation Transactions by Year, Faceted by Patron Group
+ggplot(data = circ_data, aes(x=year(Charge_Date))) +
   geom_bar() +
-  theme_minimal()
+  facet_grid(Patron ~ .)
+
+# Bar Plot of Circulation Transactions by Year, Faceted by Item Type
+ggplot(data = circ_data, aes(x=year(Charge_Date))) +
+  geom_bar() +
+  facet_grid(Item_Type ~ .)
 
 ###############################################################################
-# Transformations and Visualizations __________________________________________
+# Transformations Collapsing Factors __________________________________________
 ###############################################################################
 
-eq_circ_data <- circ_data %>%
-  filter(grepl("EQ",Item_Type)) %>%
-  filter(year(Charge_Date) >= 2014)
+# Too many patron groups; I want to collapse them.
+# Simple Example
+circ_data$Patron
 
-ggplot(eq_circ_data, aes(x=year(Charge_Date),y=count(Circ_ID))) +
-  geom_line() +
-  facet_grid(Item_Type~.,scales="free")
+fct_collapse(circ_data$Patron,
+             Student=c("Undergraduate","Graduate"),
+             Staff=c("")
+)
+
