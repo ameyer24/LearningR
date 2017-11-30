015b9b7ed9273b7f780122b0161a7c7abbfd457f
Andy Meyer <ameyer24@gmail.com>
2017-11-30 16:20:10 -0600

    Started to incorporate ebook usage data.

--- Circulation/1d - Import eBook Usage Data.R
+++
@@ -1,6 +0,0 @@
-# Path to the item informaiton file
-item.file <- ""
-# Column names for item data
-item.col.names = c()
-# This is the item information
-ill.data <- read.csv(item.file, col.names = item.col.names)
