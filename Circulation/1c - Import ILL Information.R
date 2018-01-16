05fafc06463af54f4a481765bcc114280deb9bae
Andy Meyer <ameyer24@gmail.com>
2018-01-16 16:48:36 -0600

    Deleting these files to keep this project more manageable...created new project for more holistic approach.

--- Circulation/1c - Import ILL Information.R
+++
@@ -1,6 +0,0 @@
-# Path to the item informaiton file
-ill_file <- ""
-# Column names for item data
-ill_col_names = c()
-# This is the item information
-ill_data <- read.csv(ill_file, col.names = ill_col_names)
