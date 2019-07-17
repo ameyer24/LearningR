c2e0eb8f6856d5df46b050dc42f4a577b5fb63eb
Andy Meyer <ameyer24@gmail.com>
2019-07-17 09:59:53 -0500

    Cleaning up some files.

--- Electronic Resources/0 - Setup.R
+++ Electronic Resources/0 - Setup.R
@@ -1,17 +1,16 @@
 ###############################################################################
 # Installing Packages _________________________________________________________
 ###############################################################################
-# install.packages("tidyverse")
-# install.packages("readxl")
-# install.packages("xlsx")
-# install.packages("zoo")
-# install.packages("mosaic")
-# install.packages("scales")
+install.packages("tidyverse")
+install.packages("readxl")
+install.packages("xlsx")
+install.packages("zoo")
+install.packages("mosaic")
+install.packages("scales")
 
 ###############################################################################
 # Loading Packages ____________________________________________________________
 ###############################################################################
-
 library(tidyverse)
 library(readxl)
 library(lubridate)
@@ -28,5 +27,3 @@ getwd()
 DB1_folder <- "./DB1_reports"
 JR1_folder <- "./JR1_reports"
 output_folder <- "./outputs"
-
-Test
